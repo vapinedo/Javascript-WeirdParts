ccd44cc54a751197e30e827f76a08ddf24ac65ae
Victor Pinedo <victor.pineda@linea.ai>
2023-10-14 10:15:44 -0500

    Adding our objects and its properties

--- strict-mode.js
+++ strict-mode.js
@@ -1,6 +1,13 @@
 "use strict";
 
-var person;
+// var person;
 
-persom = {};
-console.log(persom);
+// persom = {};
+// console.log(persom);
+
+function sum(a, b) {
+  // SyntaxError: "use strict" not allowed in function with default parameter
+  "use strict";
+  return a + b;
+}
+sum(3, 4);
