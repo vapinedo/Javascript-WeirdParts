098b0b2d0b172a8e705b1b249f0013c3baf3062c
Victor Pinedo <victor.pineda@linea.ai>
2023-10-20 08:08:16 -0500

    add greet library

--- greetr-library.js
+++ greetr-library.js
@@ -3,7 +3,57 @@
     return new Greetr.init(firstname, lastname, language);
   };
 
-  Greetr.prototype = {};
+  var supportedLangs = ["en", "es"];
+
+  var greetings = {
+    en: "Hello",
+    es: "Hola",
+  };
+
+  var formalGreetings = {
+    en: "Greetins",
+    es: "Saludos",
+  };
+
+  var logMessages = {
+    en: "Logged in",
+    es: "Inició Sesión",
+  };
+
+  Greetr.prototype = {
+    fullName: function () {
+      return this.firstname + " " + this.lastname;
+    },
+    validate: function () {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw "Invalid language";
+      }
+    },
+    greeting: function () {
+      return greetings[this.language] + " " + this.firstname + "!";
+    },
+    formalGreeting: function () {
+      return formalGreetings[this.language] + " " + this.fullName();
+    },
+    greet: function (formal) {
+      var msg;
+
+      // if undefined or null, then it will be coerced to false
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+
+      if (console) {
+        console.log(msg);
+      }
+
+      // 'this' refers to the calling object at execution time
+      // so, makes the method chainable
+      return this;
+    },
+  };
 
   Greetr.init = function (firstname, lastname, language) {
     var self = this;
