d7d2832fcec75bbdeb7efb408512e3a5fad6fc85
Victor Pinedo <victor.pineda@linea.ai>
2023-10-22 10:45:47 -0500

    const keyworkd

---
+++ const.js
@@ -0,0 +1,16 @@
+// const firstname = 'Jim';
+
+// {
+//     const firstname = 'Anna';
+//     console.log(firstname);
+// }
+
+// console.log(firstname)
+
+// const firstname = 'Jim';
+// firstname = 'Anna';
+// console.log(firstname);
+
+const me = { firstname: 'Jonny', lastname: 'Cage' };
+me.firstname = 'Liu'; 
+console.log(me);
